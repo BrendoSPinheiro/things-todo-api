2f51826fb5e843bb3aa77ab9aea44083005e9ed0
Brendo Souza Pinheiro <brendo.spinheiro@gmail.com>
2021-11-27 13:18:52 -0300

    chore: add and configure typeorm and pg driver

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TasksModule } from './modules/tasks/tasks.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TasksModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'root',
+      password: 'root',
+      database: 'things-todo',
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+    TasksModule,
+  ],
 })
 export class AppModule {}
